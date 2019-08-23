84fb5d9484e636c9f9c077de0eff5233c0c45abb
Priscilla Firpo <priscillayf@gmail.com>
2019-08-23 23:40:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,6 +15,7 @@ def reduce(source_array, starting_point=nil)
   else 
     total = source_array[0]
     i = 1
+  end
   while i < source_array.length do
     yield(starting_point,source_array[i])
     i += 1
