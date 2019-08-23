e640a59c2462a55a47c355e6fadb1915707b7b0c
Priscilla Firpo <priscillayf@gmail.com>
2019-08-23 23:40:52 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,11 +8,16 @@ def map(source_array)
   return new
 end
 
-def reduce(source_array, starting_point=)
-  total = starting_point
-  i = 0
+def reduce(source_array, starting_point=nil)
+  if starting_point
+    total = starting_point
+    i = 0
+  else 
+    total = source_array[0]
+    i = 1
+  end
   while i < source_array.length do
-    yield(starting_point,source_array[i])
+    total = yield(total,source_array[i])
     i += 1
   end
   total
