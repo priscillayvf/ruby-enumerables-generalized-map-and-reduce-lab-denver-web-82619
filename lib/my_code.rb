7d1378c724ad2241ea06f5895560ffe58334a2b2
Priscilla Firpo <priscillayf@gmail.com>
2019-08-23 23:35:39 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
-# Your Code Here
+def map(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push(yield(source_array[i]))
+    i += 1
+  end
+  return new
+end
+
+def reduce(source_array, starting_point=)
+  total = starting_point
+  i = 0
+  while i < source_array.length do
+    yield(starting_point,source_array[i])
+    i += 1
+  end
+  total
+end
